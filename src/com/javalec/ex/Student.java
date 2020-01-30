03869fcf3bfd246a74c6ccc7ec82059f24a00f3e
macaronjung <macaronjung@gmail.com>
2020-01-30 19:37:04 +0900

    Initial Commit

---
+++ src/com/javalec/ex/Student.java
@@ -0,0 +1,48 @@
+package com.javalec.ex;
+
+public class Student {
+	
+	private String name;
+	private int age;
+	private int grade;
+	private int studentNum;
+	
+	public Student() {
+		
+	}
+	
+	//우클릭 source - G G and S - 모두 선택 후 확인
+	//직접 타이핑 안해도 됨!!!
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public int getStudentNum() {
+		return studentNum;
+	}
+
+	public void setStudentNum(int studentNum) {
+		this.studentNum = studentNum;
+	}
+	
+}
